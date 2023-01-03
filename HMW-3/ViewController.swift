965b106bf5f0a6edd03a1e351aa9c6a111e759d0
bembi <ismanovvvabembi123>
2023-01-03 16:48:08 +0600

    Initial Commit

---
+++ HMW-3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HMW-3
+//
+//  Created by 지구9 on 3/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
